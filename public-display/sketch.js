cdd1255ceabdd23974dad41d5d94c4286e4bed6d
Your Name <you@example.com>
2023-11-16 15:55:07 -0500

    font uppload

--- public-display/sketch.js
+++ public-display/sketch.js
@@ -1,7 +1,29 @@
+let socket = io();
+
+let info = "todavia no hay nada"
+let selection = [];
+
+function preload() {
+  //Fonts
+  InterReg = loadFont('/assets/fonts/static/Inter-Regular.ttf');
+  InterLight = loadFont('/assets/fonts/static/Inter-Light.ttf');
+  InterBold = loadFont('/assets/fonts/static/Inter-Bold.ttf');
+  //Img
+  //tetrisImg = loadImage('/assets/imgs/tetris.png');
+  //Music
+  //SFX
+}
+
 function setup() {
-  createCanvas(400, 400);
+    frameRate(60)
+    createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
-  background(220);
+    background(0, 10)
+    console.log(info)
 }
+
+socket.on('confirmation', (data) => {
+    info = data
+})
